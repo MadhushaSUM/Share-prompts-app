95f17263b303eed4b62eba55a6107277b78de761
Madhusha Laksitha <madhusha.sum@gmail.com>
2023-08-22 00:21:12 +0530

    Create navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover &  Share
+            <br />
+            <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            PromptSharer is a great place for you to share your favourite A.I prompts and discover interesting prompts posted by others.
+        </p>
+
+        < Feed />
+    </section>
   )
 }
 
